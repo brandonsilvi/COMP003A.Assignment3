66308525258d8ec2eba8d25b47fc155ae46de361
Brandon Silvibarr <brandon.silvi@gmail.com>
2026-01-29 17:10:20 -0800

    sections and formatting complete

--- Program.cs
+++ Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+namespace COMP003A.Assignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Academic & Personal Readiness Evaluator ===");
+            
+            //Behavior 1 time based readiness
+            
+            
+            
+            //Behavior 2 multi rules based readiness
+            
+            
+            
+            //Mandatory rule
+            
+            
+            
+            //Behavior 3 guidance selection
+            
+            
+            
+        }
+    }
+}
